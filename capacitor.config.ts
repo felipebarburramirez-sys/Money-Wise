2185941a9152465663cc57a4e81606e6c79f2416
FelipeBarbur <felipe.barburramirez@unicolombo.edu.co>
2026-03-03 14:45:20 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Money-Wise',
+  webDir: 'www'
+};
+
+export default config;
